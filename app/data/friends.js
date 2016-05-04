990977ea0b31402b5cd32b52baa8e5aad9632d86
Aisha S Durrani <cherryblossom@Admin.local>
2016-05-04 09:26:25 -0400

    3

---
+++ app/data/friends.js
@@ -0,0 +1,25 @@
+// Friends Array that will populate with the users response
+var friendsArray =
+  [
+
+    {
+        "name":"Aisha Durrani",
+        "photo":"https://lh4.googleusercontent.com/-t7VZL0OfBHc/AAAAAAAAAAI/AAAAAAAAABM/cLLrBJ3Z0Dw/photo.jpg",
+        "scores":[
+          1,
+          5,
+          5,
+          1,
+          5,
+          1,
+          4,
+          2,
+          5,
+          5
+      ]
+    }
+
+  ];
+
+
+  module.exports = (friendsArray);
